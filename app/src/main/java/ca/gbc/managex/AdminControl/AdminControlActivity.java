ec592ba6dd037a660bafdf9ce600c0fb9a50462a
RallaAnkitha09 <ankithar0107@gmail.com>
2025-01-28 18:58:48 -0500

    commit 1

--- app/src/main/java/ca/gbc/managex/AdminControl/AdminControlActivity.java
+++ app/src/main/java/ca/gbc/managex/AdminControl/AdminControlActivity.java
@@ -1,51 +1,53 @@
 package ca.gbc.managex.AdminControl;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
-
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-import ca.gbc.managex.AdminControl.Adapter.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import ca.gbc.managex.AdminControl.Fragments.ManageEmployeeFragment;
+import ca.gbc.managex.AdminControl.Fragments.ManagePOSFragment;
 import ca.gbc.managex.R;
 
-public class AdminControlActivity extends AppCompatActivity {
+public class AdminControlActivity extends AppCompatActivity  implements BottomNavigationView
+        .OnNavigationItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
 
-    TabLayout tabLayout;
-    ViewPager2 viewPager2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_control);
 
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
 
-        tabLayout = findViewById(R.id.tabs);
-        viewPager2 = findViewById(R.id.viewPager2);
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+    }
+    ManageEmployeeFragment firstFragment = new ManageEmployeeFragment();
+    ManagePOSFragment secondFragment = new ManagePOSFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),getLifecycle());
-        viewPager2.setAdapter(adapter);
 
-        TabLayoutMediator mediator = new TabLayoutMediator(tabLayout, viewPager2, true, true, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                switch (position){
-                    case 0:
-                        tab.setText("Manage POS");
-                        break;
-                    case 1 :
-                        tab.setText("Manage Employee");
-                        break;
-                }
-            }
-        });
-        mediator.attach();
+
+        if(item.getItemId() == R.id.person){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, firstFragment)
+                    .commit();
+            return true;
+        }
+        else if(item.getItemId() == R.id.pos){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, secondFragment)
+                    .commit();
+            return true;
+        }
+        return false;
 
     }
 }
---
+++ app/src/main/java/ca/gbc/managex/AdminControl/EmployeeInfoActivity.java
@@ -0,0 +1,25 @@
+package ca.gbc.managex.AdminControl;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import ca.gbc.managex.R;
+
+public class EmployeeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_employee_info);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
